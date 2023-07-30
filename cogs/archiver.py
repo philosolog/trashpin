ec271b4617c172d19bd03c2b938313d42137ddb4
philosolog <philosolog@gmail.com>
2023-07-30 00:28:28 -0700

    embed refactor

--- cogs/archiver.py
+++ cogs/archiver.py
@@ -6,34 +6,26 @@ from discord.ext import commands
 class archiver(discord.Cog, name="archiver"):
 	def __init__(self, bot):
 		self.bot = bot
-
 	@commands.Cog.listener()
 	async def on_message_edit(self, before, after): # TODO: Refactor this method...
 		if after.pinned == True:
-			name = after.author.display_name
-			avatar = after.author.display_avatar.url
-			pin_content = after.content
-			server = after.guild.id
-			current_date = datetime.datetime.utcfromtimestamp(int(time.time()))
-			emb = discord.Embed(
-				description=pin_content,
+			embed = discord.Embed(
+				description=after.content,
 				color=0x7289da,
-				timestamp=current_date
+				timestamp=datetime.datetime.utcfromtimestamp(int(time.time()))
 			)
-			emb.set_author(
-				name=name,
-				icon_url=avatar,
-				# url='https://discordapp.com/channels/{0}/{1}/{2}'.format(server, after.channel.id, after.id)
+			embed.set_author(
+				name=after.author.display_name,
+				icon_url=after.author.display_avatar.url,
 			)
 
 			if after.attachments:
-				img_url = after.attachments[0].url
-				emb.set_image(url=img_url)
+				embed.set_image(url=after.attachments[0].url)
 
-			emb.set_footer(text='Sent in #{}'.format(after.channel))
+			embed.set_footer(text='#{}'.format(after.channel))
 
 			channel = self.bot.get_channel(1134917801732227072) # TODO: Save channels per server.
-			await channel.send(content=f"https://discord.com/channels/{server}/{after.channel.id}/{after.id} by {after.author.mention}", embed=emb, silent=True) # TODO: Ping who pinned; RETHINK if it is necessary for silent=True...
+			await channel.send(content=f"https://discord.com/channels/{after.guild.id}/{after.channel.id}/{after.id} by {after.author.mention}", embed=embed, silent=True) # TODO: Ping who pinned; RETHINK if it is necessary for silent=True...
 
 
 def setup(bot):
